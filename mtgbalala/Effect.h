e11c50d65825e53f0d196a07b178683f3337f18d
Lefterache Ștefan <lefterache.stefan.8@gmail.com>
2026-03-07 21:29:55 +0200

    bruh

--- mtgbalala/Effect.h
+++ mtgbalala/Effect.h
@@ -1,39 +1,41 @@
 #pragma once
+#include <iostream>
 #include <memory>
+
 #include "IEffect.h"
-class RoundTracker; 
+#include "RoundTracker.h"
 class DrawCardEffect : public IEffect {
-    private:
-        int amount;
+   private:
+	int amount;
 
-    public:
-        explicit DrawCardEffect(int amt) : amount(amt) {}
+   public:
+	explicit DrawCardEffect(int amt) : amount(amt) {}
 
-        void resolve(RoundTracker& state) override {
-            for(int i = 0; i < amount; i++) {
-                state.drawCard();
-            }
-        }
+	void resolve(RoundTracker& state) override {
+		for (int i = 0; i < amount; i++) {
+			state.drawCard();
+		}
+	}
 
-        std::unique_ptr<IEffect> clone() const override {
-            return std::make_unique<DrawCardEffect>(*this); 
-        }
+	std::unique_ptr<IEffect> clone() const override {
+		return std::make_unique<DrawCardEffect>(*this);
+	}
 };
 
 class AddManaEffect : public IEffect {
-private:
-    int red, blue, green;
+   private:
+	int red, blue, green;
 
-public:
-    AddManaEffect(int r, int b, int g) : red(r), blue(b), green(g) {}
+   public:
+	AddManaEffect(int r, int b, int g) : red(r), blue(b), green(g) {}
 
-    void resolve(RoundTracker& state) override {
-        //state.addMana(red, blue, green);
-        ///TODO: mana system
-        std::cout << "Adding Mana -> R:" << red << " B:" << blue << " G:" << green << "\n";
-    }
+	void resolve(RoundTracker& state) override {
+		// state.addMana(red, blue, green);
+		/// TODO: mana system
+		std::cout << "Adding Mana -> R:" << red << " B:" << blue << " G:" << green << "\n";
+	}
 
-    std::unique_ptr<IEffect> clone() const override {
-        return std::make_unique<AddManaEffect>(*this);
-    }
+	std::unique_ptr<IEffect> clone() const override {
+		return std::make_unique<AddManaEffect>(*this);
+	}
 };
