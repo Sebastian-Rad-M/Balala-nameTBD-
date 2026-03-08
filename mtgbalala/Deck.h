e0dfdcb988c448a8b766378c1e8b8f7bf94d8bd1
Lefterache Ștefan <lefterache.stefan.8@gmail.com>
2026-03-08 15:08:58 +0200

    case fix

--- mtgbalala/Deck.h
+++ mtgbalala/Deck.h
@@ -1,15 +1,14 @@
 #pragma once
+#include <algorithm>
 #include <memory>
 #include <random>
-#include <algorithm>
-#include "Cardzone.h"
+
+#include "CardZone.h"
 #include "RNG.h"
 
 class Deck : public CardZone {
    public:
-	void shuffle() {
-		std::shuffle(cards.begin(), cards.end(), RNG::engine());
-	}
+	void shuffle() { std::shuffle(cards.begin(), cards.end(), RNG::engine()); }
 
 	std::shared_ptr<Card> popTopCard() {
 		if (cards.empty()) {
